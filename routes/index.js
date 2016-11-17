0fff1ed4b8a3dec99f260bbae0795cf52b06a750
James Kolsby <jrkolsby@mac.com>
2016-11-17 16:53:19 -0500

    Built Jade template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var data = {
+    name: "James Kolsby",
+    links: [
+        {
+            title: "Dribbble",
+            location: "https://dribbble.com/jrkolsby",
+            color: "ea4c89"
+        },
+        {
+            title: "Vimeo",
+            location: "https://vimeo.com/jameskolsby",
+            color: "1ab7ea"
+        },
+        {
+            title: "Github",
+            location: "https://github.com/jrkolsby",
+            color: "2d2d2d"
+        }
+    ]
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', data);
 });
 
 module.exports = router;
